799ab1b9d1f47aafd52ad106cfb879c355066e36
Asta <70468074+F4sta@users.noreply.github.com>
2025-03-05 22:37:41 +0100

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ config();
 
 const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:4000", "*", "https://animestream-ten.vercel.app/"];
+  : ["http://localhost:4000", "*", "https://animestream-ten.vercel.app/", "http://localhost:5500", "http://localhost"];
 
 const corsConfig = cors({
   allowMethods: ["GET"],
